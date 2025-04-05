ad854794f60010f667d02288569da89b74757143
choviZ <702381764@qq.com>
2025-04-05 16:21:32 +0800

    后端初始化

---
+++ dabo-backend/src/main/java/com/zcw/dabo/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.zcw.dabo.controller;
+
+import com.zcw.dabo.common.BaseResponse;
+import com.zcw.dabo.common.ErrorCode;
+import com.zcw.dabo.common.ResultUtils;
+import com.zcw.dabo.exception.BusinessException;
+import com.zcw.dabo.model.dto.postthumb.PostThumbAddRequest;
+import com.zcw.dabo.model.entity.User;
+import com.zcw.dabo.service.PostThumbService;
+import com.zcw.dabo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 帖子点赞接口
+ *
+ 
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+                                         HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
